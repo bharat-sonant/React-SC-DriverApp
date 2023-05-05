08a84bec7a83e50c6e461f0f2e50d3d9ad60eb57
shubhamsoni <shubhamsoni.wevois@gmail.com>
2023-05-05 17:08:46 +0530

    code committed for login design

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,4 +1,4 @@
-import { ActivityIndicator, Alert, BackHandler, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, Alert, BackHandler, Button, Dimensions, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 // import { ref } from 'firebase/database';
 import { database } from '../Firebase';
@@ -7,6 +7,8 @@ import { TextInput } from 'react-native-gesture-handler';
 import { NavigationAction, useNavigation } from '@react-navigation/native';
 import { useBackHandler } from '@react-native-community/hooks';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 const Login = () => {
     const [mobileNumber, setMobileNumber] = useState('8786786691');
     const [pin, setPin] = useState('302021');
@@ -56,31 +58,50 @@ const Login = () => {
         })
     }
     return (
+
         <View style={styles.container}>
-            <ActivityIndicator style={styles.ActivityIndicatorLayout} size={'large'} animating={show} color={'black'} />
-            <Image style={styles.image} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS42EsAolPV2NGnQO1X87f7uSZGobRNUO7SiW-qVaCLlTzmqkuuKtq8RogeO8WFJawZXxQ&usqp=CAU" }} />
-            <View style={{ flexDirection: 'row', width: "70%", borderWidth: 1, borderRadius: 15, borderColor: 'grey', marginTop: 20, paddingLeft: 5, paddingTop: 8, paddingBottom: 8 }} >
-                <Image style={{ width: 35, height: 35, borderRadius: 45, marginLeft: 3, marginRight: 10, marginTop: 5 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBBpjKV5f6Bu_km648nAq7du23ZdxgvAojNzs3RK79qG1p35IAmlVr0xVEC3-iYiXTpYw&usqp=CAU" }} />
-                <TextInput keyboardType="numeric" maxLength={10} placeholder='Emp Id' style={{ width: "100%", color: 'black' }}
-                    value={mobileNumber}
-                    onChangeText={setMobileNumber}
+
+           
+            <Image source={{ uri: "https://www.gwcindia.in/assets/backoffice.png", height: 300, width: Dimensions.get('screen').width }} />
+    
+            <View style={styles.inputStyle}>
+             
+                <Image style={styles.icon} source={{ uri: "https://static.vecteezy.com/system/resources/thumbnails/007/033/146/small/profile-icon-login-head-icon-vector.jpg" }} />
+
+               
+                <TextInput keyboardType="numeric" placeholder='Number' style={{ color: "black", fontSize: 15, marginBottom: -10 }}
+                value={mobileNumber}
+                onChangeText={setMobileNumber}
+                 />
+
+            </View>
+            <View style={styles.inputStyle}>
+               
+
+                <Image style={styles.imageStyl} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8500uKjsVedMStg1isnwiq9CArOWUVwLLzwanyk5gp0DWPxIWmAtTfYMafLzWMq9xeak&usqp=CAU" }} />
+          
+                <TextInput placeholder='Password' style={{ color: "black", fontSize: 15, marginBottom: -10 }} 
+                 value={pin}
+                 onChangeText={setPin}
+                 secureTextEntry={true}
                 />
             </View>
-            <View style={{ flexDirection: 'row', width: "70%", borderWidth: 1, borderRadius: 15, borderColor: 'grey', marginTop: 20, paddingLeft: 5, paddingTop: 8, paddingBottom: 8 }} >
-                <Image style={{ width: 35, height: 35, borderRadius: 40, marginRight: 10, marginTop: 5, marginLeft: 3 }} source={{ uri: "https://www.shutterstock.com/image-vector/password-security-symbol-lock-icon-260nw-782799754.jpg" }} />
-                <TextInput placeholder='Password' style={{ width: "100%", color: 'black' }}
-                    value={pin}
-                    onChangeText={setPin}
-                    secureTextEntry={true}
-                />
+           
+            <View style={{ justifyContent: "center", alignItems: 'center' }}>
+                <TouchableOpacity style={styles.buttonStyle} onPress={() => getLoginData()} >
+                    <Text style={{ color: "#fff", fontSize: 16, fontWeight: '700', marginTop: -5 }}>Login</Text>
+                </TouchableOpacity>
+
             </View>
-            <TouchableOpacity onPress={() => getLoginData()} >
-                <View style={{ borderRadius: 15, borderWidth: 1, padding: 15, marginTop: 25, width: 165, backgroundColor: 'grey' }} >
-                    <Text style={styles.loginText}>LOGIN</Text>
 
-                </View>
-            </TouchableOpacity>
 
+
+            {/* <View >
+                <Text style={{
+                    textAlign: 'center',
+                    textAlignVertical: 'center'
+                }}>Don't have an account? <Text style={{ color: '#00a2ed' }} >Sign up</Text> </Text>
+            </View> */}
 
         </View>
 
@@ -91,9 +112,18 @@ export default Login
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 140,
+        // width:'100%'
+        // marginTop: -150,
+        flex: 1,
+        // height: '100%',
+        // flex: 1,
+        // flexDirection: 'row',
+        backgroundColor: '#fff',
         justifyContent: 'center',
-        alignItems: 'center',
+        // alignItems: 'center',
+        // backgroundColor: 'black'
+
+
     },
     loginBtn: {
         borderWidth: 1,
@@ -113,10 +143,10 @@ const styles = StyleSheet.create({
         fontWeight: '700'
 
     }, image: {
-        width: 120,
-        height: 120,
+        width: '100%',
+        height: 60,
         marginBottom: 20,
-        borderRadius: 75,
+        // borderRadius:75 ,
         borderWidth: 1,
         borderColor: 'green'
     },
@@ -129,10 +159,61 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     }, ActivityIndicatorLayout: {
         position: 'absolute',
+
         left: "45%",
         top: "90%",
         zIndex: 1
-    }
+    },
+    inputStyle: {
+        //    height:70,
+        borderColor: "#000",
+        borderBottomWidth: 0.5,
+        marginHorizontal: 30,
+        marginVertical: 8,
+        // margin:80
+        paddingLeft: 23,
+        paddingTop: 10,
+        _justifyContent: 'center',
+
+        // alignItems:'center'
+
+    },
+    buttonStyle: {
+        height: 45,
+        width: '85%',
+        backgroundColor: '#00a2ed',
+        // marginHorizontal: 40,
+        marginVertical: 30,
+        borderRadius: 4,
+        justifyContent: 'center',
+        // marginHorizontal:20,
+
+        alignItems: 'center'
+
+
+
+    },
+    imageStyl: {
+        position: 'absolute',
+        width: 20,
+        height: 23,
+        bottom: 3,
+        left: 0,
+        // right:60
+        borderRadius: 10
+
+
+    },
+    icon: {
+        position: 'absolute',
+        width: 20,
+        height: 20,
+        bottom: 3,
+        left: 0,
+        // right:60
+        borderRadius: 10
+    },
+   
 
 
 })
