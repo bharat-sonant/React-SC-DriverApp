61807f0e0acf4452c1495ffc926c0845fb8935a7
VickyKumar02 <mk6144791@gmail.com>
2023-06-14 12:12:30 +0530

    code committed

--- App.js
+++ App.js
@@ -56,67 +56,6 @@ const App = () => {
           }}
         />
       </Tab.Navigator>
-
-      // <Tab.Navigator
-      //   screenOptions={({ route }) => ({
-      //     headerShown: false,
-      //     tabBarStyle: {
-      //       height: 50,
-      //     },
-      //     tabBarIcon: ({ focused, size, colour }) => {
-      //       let iconName;
-      //       if (route.name === "Home") {
-      //         iconName = focused ? "home" : "home-outline";
-      //         size = focused ? size + 7 : size + 4;
-      //       } else if (route.name === "Profile") {
-      //         iconName = focused
-      //           ? "person-circle-sharp"
-      //           : "person-circle-outline";
-      //         size = focused ? size + 11 : size + 8;
-      //       } else if (route.name == "Complaints") {
-      //         iconName = focused ? "md-person-sharp" : "person-outline";
-      //         size = focused ? size + 7 : size + 2;
-      //       }
-      //       return <Ionic name={iconName} size={size} color={colour} />;
-      //     },
-      //   })}
-      // >
-      //   <Tab.Screen name="Home" component={Home} />
-      //   <Tab.Screen name="Profile" component={Logout} />
-      //   <Tab.Screen name="Complaints" component={MapScreen} />
-      // </Tab.Navigator>
-
-
-
-
-
-
-      // <Tab.Navigator
-      //   screenOptions={{ headerShown: false, statusBarColor: "fade" }}
-      // >
-      //   <Tab.Screen name="Calculation" component={Home}
-      //     options={{
-      //       tabBarLabel: 'Calculation',
-      //       tabBarIcon: ({ color, size }) => (
-      //         // <Ionicons name="leftcircle" color={color} size={size} />
-      //         <Ionicons name="calculator" size={size} color={color}></Ionicons>
-      //       ),
-      //     }}
-      //   />
-      //   <Tab.Screen name="Ratelist" component={Logout} options={{
-      //     tabBarLabel: 'Rate List',
-      //     tabBarIcon: ({ color, size }) => (
-
-      //       <Ionicons name="pricetags" color={color} size={size} />
-      //     ),
-      //   }} />
-      //   <Tab.Screen name="Travel" component={MapScreen} options={{
-      //     tabBarLabel: 'Travel',
-      //     tabBarIcon: ({ color, size }) => (
-      //       <Ionicons name="navigate" color={color} size={size} />
-      //     ),
-      //   }} />
-      // </Tab.Navigator>
     );
   };
 
